7f5a752ecd0dc8e59b763b10783c90006c976ffd
Suporte Spectra <suporte@spectra.tec.br>
2026-01-07 21:49:43 -0300

    Filtrando Cursos
    Co-Authored-By: Gustavo P. Santana <58692651+gustavopsan@users.noreply.github.com>

--- src/pages/educatorsPage.tsx
+++ src/pages/educatorsPage.tsx
@@ -22,6 +22,7 @@ function EducatorsPage() {
     const [courses, setCourses] = useState<TCourse[]>([]);
     const [statusFilter, setStatusFilter] = useState("");
     const [orderFilter, setOrderFilter] = useState("");
+    const [search, setSearch] = useState("");
 
         useEffect(() => {    
             async function fetchCourses() {
@@ -52,12 +53,21 @@ function EducatorsPage() {
             if (statusFilter === "finshed") return course.progressPercentage === 100;
             return true;
         })
+        .filter(course => {
+            if (!search) return true;
+
+            return (
+            course.title.toLowerCase().includes(search.toLowerCase()) ||
+            course.subTitle?.toLowerCase().includes(search.toLowerCase())
+            );
+        })
         .sort((a, b) => {
             if (orderFilter === "crescent") return a.title.localeCompare(b.title);
             if (orderFilter === "decrescent") return b.title.localeCompare(a.title);
             if (orderFilter === "progress") return b.progressPercentage - a.progressPercentage;
             return 0;
         });
+
 
     return (
         <React.Fragment>
@@ -70,7 +80,7 @@ function EducatorsPage() {
 
                             <div className="filters-wrapper">
                                 <div className="search-wrapper">
-                                    <input type="search" className="course-search" id="courseSearchInput" placeholder="Procure o curso" />
+                                    <input type="search" className="course-search" id="courseSearchInput" placeholder="Procure o curso" value={search} onChange={(e) => setSearch(e.target.value)} />
                                     <button disabled>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 21 21" fill="none">
                                             <path d="M13.6999 2.34726C10.5702 -0.782421 5.47644 -0.782421 2.34675 2.34726C-0.782251 5.47762 -0.782251 10.5707 2.34675 13.7011C5.13382 16.4875 9.47453 16.786 12.6022 14.6102C12.668 14.9216 12.8186 15.2188 13.0608 15.461L17.6186 20.0188C18.2828 20.6817 19.3561 20.6817 20.0169 20.0188C20.6805 19.3553 20.6805 18.282 20.0169 17.6205L15.4591 13.0613C15.2183 12.8212 14.9204 12.6699 14.609 12.604C16.7862 9.47572 16.4877 5.13569 13.6999 2.34726ZM12.2609 12.2621C9.92435 14.5987 6.12164 14.5987 3.78574 12.2621C1.45052 9.92553 1.45052 6.12351 3.78574 3.78693C6.12164 1.45103 9.92435 1.45103 12.2609 3.78693C14.5975 6.12351 14.5975 9.92553 12.2609 12.2621Z" fill="black"/>
