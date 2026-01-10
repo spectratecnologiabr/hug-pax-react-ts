2839b1b15471bf338213d395db1bb69ec4968263
Suporte Spectra <suporte@spectra.tec.br>
2026-01-09 23:31:51 -0300

    Iniciando telas de administração
    Co-Authored-By: Gustavo P. Santana <58692651+gustavopsan@users.noreply.github.com>

--- src/pages/adminDash.tsx
+++ src/pages/adminDash.tsx
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getOverviewData } from "../controllers/dash/overview.controller";
+
+import PageSelector from "../components/pageSelector";
+import Menubar from "../components/admin/menubar";
+
+import "../style/adminDash.css";
+
+type TOverviewData = {
+    completedCourses: number,
+    inProgressCourses: number,
+    totalHours: number,
+    unreadNotifications: number
+}
 
 function AdminDash() {
+    const [ overviewData, setOverviewData ] = useState<TOverviewData | null>(null);
+
+    useEffect(() => {
+        async function fetchOverviewData() {
+            try {
+                const overviewData = await getOverviewData();
+                setOverviewData(overviewData);
+            } catch (error) {
+                console.error("Error fetching overview data:", error);
+            }
+        }
+
+        fetchOverviewData()
+    }, []);
+
     return (
         <React.Fragment>
-            <h1>Admin Dashboard</h1>
+            <div className="admin-dashboard-container">
+                <Menubar notificationCount={Number(overviewData?.unreadNotifications)}/>
+
+            </div>
+            <PageSelector />
         </React.Fragment>
     )
 }
